d9a39e1d31510568cb03a1f13ad09beb4820681a
SANYAM CHAUDHARY <104439088+SANYAMCHAUDHARY22@users.noreply.github.com>
2024-02-04 08:48:11 +0530

    Time: 134 ms (41.06%), Space: 17.4 MB (55.90%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        lookup = Counter(t)
+        mx = float("inf")
+        output = ""
+        S = len(s)
+        start, end = 0, 0
+        count = len(lookup)
+        
+        while end < S:
+            
+            # End Pointer
+            while end < S and count != 0:
+                if s[end] in lookup:
+                    lookup[s[end]] -= 1
+                    if lookup[s[end]] == 0:
+                        count -=1
+                end += 1
+                
+            # Start Pointer
+            while start <= end and count == 0:
+                
+                if end-start < mx:
+                    mx = end - start
+                    output = s[start:end]
+                
+                if s[start] in lookup:
+                    lookup[s[start]] += 1
+                    if lookup[s[start]] > 0:
+                        count += 1
+                
+                start += 1
+        
+        return output
