ad4649909cfc9fa2b13a098a1d11f009cb9839fc
SANYAM CHAUDHARY <104439088+SANYAMCHAUDHARY22@users.noreply.github.com>
2024-02-08 14:26:33 +0530

    Time: 41 ms (98.58%), Space: 16.5 MB (92.57%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isSquare(self, n: int) -> bool:
+        sq = int(math.sqrt(n))
+        return sq*sq == n
+        
+    def numSquares(self, n: int) -> int:
+        # Lagrange's four-square theorem
+        if self.isSquare(n):
+            return 1
+        while (n & 3) == 0:
+            n >>= 2
+        if (n & 7) == 7:
+            return 4
+        sq = int(math.sqrt(n)) + 1
+        for i in range(1,sq):
+            if self.isSquare(n - i*i):
+                return 2
+        return 3
