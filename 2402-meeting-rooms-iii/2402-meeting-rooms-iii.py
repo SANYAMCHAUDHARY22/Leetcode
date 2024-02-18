4cf6e2dff0ad36a1a62d82485709c76c914414e8
SANYAM CHAUDHARY <104439088+SANYAMCHAUDHARY22@users.noreply.github.com>
2024-02-18 18:35:46 +0530

    Time: 1152 ms (90.82%), Space: 62.3 MB (83.25%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        booked, free, res = [], list(range(n)), [0] * n
+        meetings.sort()
+
+        for start, end in meetings:
+            while booked and booked[0][0] <= start:
+                _, room = heappop(booked)
+                heappush(free, room)
+            
+            if free:
+                room = heapq.heappop(free)
+                heappush(booked, (end, room))
+            else:
+                release, room = heappop(booked)
+                heappush(booked, (release + (end - start), room))
+            
+            res[room] += 1
+
+        return res.index(max(res))
