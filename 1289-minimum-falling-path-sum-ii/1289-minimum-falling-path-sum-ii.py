cf1d01c36e51594b729da41b2ff788e476ab8344
SANYAM CHAUDHARY <104439088+SANYAMCHAUDHARY22@users.noreply.github.com>
2024-04-26 22:29:44 +0530

    Time: 215 ms (94.69%), Space: 20.2 MB (21.24%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minFallingPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        N = len(grid)
+        DP = grid[0]
+
+        for i in range(1, N):
+            indx1 = DP.index(min(DP))
+            indx2 = DP.index(min(DP[:indx1] + DP[indx1+1:]))
+            for j in range(N):
+                if j != indx1:
+                    grid[i][j] += DP[indx1]
+                else:
+                    grid[i][j] += DP[indx2]
+            DP = grid[i]
+
+        return min(DP)
