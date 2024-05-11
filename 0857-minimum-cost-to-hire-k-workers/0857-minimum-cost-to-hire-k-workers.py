b9bae53ceb04f96b428a753ba17fa727955fa4d5
SANYAM CHAUDHARY <104439088+SANYAMCHAUDHARY22@users.noreply.github.com>
2024-05-11 21:26:07 +0530

    Time: 144 ms (93.18%), Space: 18.9 MB (52.82%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        # 9:37
+        total_wage, total_quality, max_heap = float('inf'), 0, []
+        for ratio, q in sorted([(w/q, q) for w, q in zip(wage, quality)]):
+            total_quality += q
+            heappush(max_heap, -q)
+            if len(max_heap) == k:
+                total_wage = min(total_wage, total_quality * ratio)
+                total_quality += heappop(max_heap)
+        return total_wage
